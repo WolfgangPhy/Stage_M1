125aa775141129f06f1d87da9cfd806a5c7031ce
WolfgangPhy <antonin937@gmail.com>
2024-02-05 14:11:31 +0100

    Clarification du code pour ext_model.py

--- Baseressources/ext_model.py
+++ Baseressources/ext_model.py
@@ -5,7 +5,7 @@ from scipy.spatial.transform import Rotation as R
 
 #utility functions for coordinate change and integration
 
-def lbd_xyz(l,b,d):
+def ConvertGalacticToCartesian3D(l, b, d):
     """Converts from galactic coordinates to cartesian coordinates
 
     Args:
@@ -16,9 +16,11 @@ def lbd_xyz(l,b,d):
     Returns:
         tuple[float, float, float] : Cartesian coordinates (x,y,z) in kpc
     """
-    return d*math.cos(b*math.pi/180.)*math.cos(l*math.pi/180.),d*math.cos(b*math.pi/180.)*math.sin(l*math.pi/180.),d*math.sin(b*math.pi/180.)
+    return  d * math.cos(b*math.pi/180.) * math.cos(l*math.pi/180.), \
+            d * math.cos(b*math.pi/180.) * math.sin(l*math.pi/180.), \
+            d * math.sin(b*math.pi/180.)
 
-def ld_xy(l,d):
+def ConvertGalacticToCartesian2D(l, d):
     """Converts from galactic coordinates to cartesian coordinates
 
     Args:
@@ -28,9 +30,10 @@ def ld_xy(l,d):
     Returns:
         tuple[float, float]: Cartesian coordinates (x,y) in kpc
     """
-    return d*math.cos(l*math.pi/180.),d*math.sin(l*math.pi/180.)
+    return  d * math.cos(l*math.pi/180.), \
+            d * math.sin(l*math.pi/180.)
 
-def xyz_lbd(x,y,z):
+def ConvertCartesianToGalactic3D(x, y, z):
     """Converts from cartesian coordinates to galactic coordinates
 
     Args:
@@ -41,13 +44,15 @@ def xyz_lbd(x,y,z):
     Returns:
         tuple[float, float, float]: Galactic coordinates (l,b,d) in degrees and kpc
     """
-    R = math.sqrt(x**2+y**2)
+    R = math.sqrt(x**2 + y**2)
     ell = math.atan2(y,x)
-    if ell<0:
+    if ell < 0:
         ell = ell + 2.*math.pi
-    return ell*180./math.pi,math.atan2(z,R)*180./math.pi,math.sqrt(x**2+y**2+z**2)
+    return  ell*180./math.pi, \
+            math.atan2(z,R) * 180./math.pi, \
+            math.sqrt(x**2 + y**2 + z**2)
 
-def xy_ld(x,y):
+def ConvertCartesianToGalactic2D(x, y):
     """Converts from cartesian coordinates to galactic coordinates
 
     Args:
@@ -57,18 +62,18 @@ def xy_ld(x,y):
     Returns:
         tuple[float, float]: Galactic coordinates (l,d) in degrees and kpc
     """
-    R = math.sqrt(x**2+y**2)
-    ell = math.atan2(y,x)
-    if ell<0:
+    R = math.sqrt(x**2 + y**2)
+    ell = math.atan2(y, x)
+    if ell < 0:
         ell = ell + 2.*math.pi
-    return ell*180./math.pi,R
+    return  ell * 180./math.pi, \
+            R
 
-def integ_d(f,l,b,dmax,model,dd=0.01):
+def integ_d(func , l, b, dmax, model, dd=0.01):
     """Integrates a function f over a line of sight in the galactic plane
     
-
     Args:
-        f (function): Function to integrate
+        func (function): Function to integrate
         l (float): Galactic longitude in degrees (0 to 360)
         b (float): Galactic latitude in degrees (-90 to 90)
         dmax (float): Maximum distance in kpc
@@ -80,25 +85,24 @@ def integ_d(f,l,b,dmax,model,dd=0.01):
     """
     #uses trapezoidal rule WARNING - dmax/dd might not be an integer
     n = int(dmax/dd)
-    x,y,z = lbd_xyz(l,b,dmax)
-    s = 0.5*(f(0.,0.,0.,model) + f(x,y,z,model))
-    for i in range(1,n,1):
-        x,y,z = lbd_xyz(l,b,i*dd)
-        s = s + f(x,y,z,model)
-    return dd*s
+    x, y, z = ConvertGalacticToCartesian3D(l, b, dmax)
+    s = 0.5 * (func(0., 0., 0., model) + func(x, y, z, model))
+    for i in range(1, n, 1):
+        x, y, z = ConvertGalacticToCartesian3D(l, b, i*dd)
+        s = s + func(x, y, z, model)
+    return dd * s
 
 def integ_d_async(idx,f,l,b,dmax,model,dd=0.01):
     #uses trapezoidal rule WARNING - dmax/dd might not be an integer
     n = int(dmax/dd)
-    x,y,z = lbd_xyz(l,b,dmax)
+    x,y,z = ConvertGalacticToCartesian3D(l,b,dmax)
     s = 0.5*(f(0.,0.,0.,model) + f(x,y,z,model))
     for i in range(1,n,1):
-        x,y,z = lbd_xyz(l,b,i*dd)
+        x,y,z = ConvertGalacticToCartesian3D(l,b,i*dd)
         s = s + f(x,y,z,model)
     return (idx,dd*s)
 
-# construct the extinction model we will try to recover and save it to a file
-def gauss3d(x,y,z,x0,y0,z0,rho,s1,s2,s3,a1,a2):
+def gauss3d(x, y, z, x0, y0, z0, rho, s1, s2, s3, a1, a2):
     """3D Gaussian function
 
     Args:
@@ -118,16 +122,15 @@ def gauss3d(x,y,z,x0,y0,z0,rho,s1,s2,s3,a1,a2):
     Returns:
         float : Value of the Gaussian function
     """
-    v=[x-x0,y-y0,z-z0]
+    v=[x-x0, y-y0, z-z0]
     r1 = R.from_euler('x', a1, degrees=True)
     r2 = R.from_euler('z', a2, degrees=True)
     xx=r2.apply(r1.apply(v))
-    return rho/((2*math.pi)**1.5*s1*s2*s3)*math.exp(-0.5*(xx[0]**2/s1+xx[1]**2/s2+xx[2]**2/s3))    
+    return rho/((2*math.pi)**1.5 * s1 * s2 * s3) * math.exp(-0.5 * (xx[0]**2/s1 + xx[1]**2/s2 + xx[2]**2/s3))    
       
-def ext_model(x,y,z,model):
+def ext_model(x, y, z, model):
     """Computes the extinction model
     
-
     Args:
         x (float): x coordinate in kpc
         y (float): y coordinate in kpc
@@ -137,13 +140,18 @@ def ext_model(x,y,z,model):
     Returns:
         float : Value of the extinction model
     """
+    #Assumes a double exponential disk model near the Sun
+    hr = 2.5 #kpc
+    hz = 0.05 #kpc
+    absorp = 0.2 #mag/kpc
+    r_sun = 8.
     # first assumes a double exponential disk model with hr=2.5kpc hz=0.05 kpc 
     # with an absoption of 0.2mag/kpc near the Sun radius (-8,0,0)
-    R = math.sqrt((x-8.)**2 + y**2)
-    d = 0.2 * math.exp(-(R-8.)/2.5)*math.exp(-abs(z)/0.05)
+    R = math.sqrt((x-r_sun)**2 + y**2)
+    d = absorp * math.exp(-(R - r_sun)/hr) * math.exp(-abs(z)/hz)
      
     for i in range(len(model.x0)):
-        d = d + gauss3d(x,y,z,model.x0[i],model.y0[i],model.z0[i],model.rho[i],model.s1[i],model.s2[i],model.s3[i],model.a1[i],model.a2[i])
+        d = d + gauss3d(x, y, z, model.x0[i], model.y0[i], model.z0[i], model.rho[i], model.s1[i], model.s2[i], model.s3[i], model.a1[i], model.a2[i])
     
     return d
 
