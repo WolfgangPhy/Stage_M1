9c90d550714a23cf1e17bbb6577e53db56b40b4f
WolfgangPhy <antonin937@gmail.com>
2024-04-02 10:05:33 +0200

    _

--- NewPrograms/FileHelper.py
+++ NewPrograms/FileHelper.py
@@ -66,21 +66,21 @@ class FileHelper:
         
         with open(os.path.join(test_directory, 'Config.json')) as param_file:
             config_data = json.load(param_file)
-            config_data['outfile'] = os.path.join('./Tests', test_directory, config_data['outfile'][2:])
-            config_data['logfile'] = os.path.join('./Tests', test_directory, config_data['logfile'][2:])
-            config_data['lossfile'] = os.path.join('./Tests', test_directory, config_data['lossfile'][2:])
-            config_data['valfile'] = os.path.join('./Tests', test_directory, config_data['valfile'][2:])
-            config_data['ext_grid_file'] = os.path.join('./Tests', test_directory, config_data['ext_grid_file'][2:])
-            config_data['dens_grid_file'] = os.path.join('./Tests', test_directory, config_data['dens_grid_file'][2:])
-            config_data['ext_los_file'] = os.path.join('./Tests', test_directory, config_data['ext_los_file'][2:])
-            config_data['dens_los_file'] = os.path.join('./Tests', test_directory, config_data['dens_los_file'][2:])
-            config_data['density_plot'] = os.path.join('./Tests', test_directory, config_data['density_plot'][2:])
-            config_data['density_los_plot'] = os.path.join('./Tests', test_directory, config_data['density_los_plot'][2:])
-            config_data['extinction_plot'] = os.path.join('./Tests', test_directory, config_data['extinction_plot'][2:])
-            config_data['extinction_los_plot'] = os.path.join('./Tests', test_directory,
+            config_data['outfile'] = os.path.join('./', test_directory, config_data['outfile'][2:])
+            config_data['logfile'] = os.path.join('./', test_directory, config_data['logfile'][2:])
+            config_data['lossfile'] = os.path.join('./', test_directory, config_data['lossfile'][2:])
+            config_data['valfile'] = os.path.join('./', test_directory, config_data['valfile'][2:])
+            config_data['ext_grid_file'] = os.path.join('./', test_directory, config_data['ext_grid_file'][2:])
+            config_data['dens_grid_file'] = os.path.join('./', test_directory, config_data['dens_grid_file'][2:])
+            config_data['ext_los_file'] = os.path.join('./', test_directory, config_data['ext_los_file'][2:])
+            config_data['dens_los_file'] = os.path.join('./', test_directory, config_data['dens_los_file'][2:])
+            config_data['density_plot'] = os.path.join('./', test_directory, config_data['density_plot'][2:])
+            config_data['density_los_plot'] = os.path.join('./', test_directory, config_data['density_los_plot'][2:])
+            config_data['extinction_plot'] = os.path.join('./', test_directory, config_data['extinction_plot'][2:])
+            config_data['extinction_los_plot'] = os.path.join('./', test_directory,
                                                               config_data['extinction_los_plot'][2:]
                                                               )
-            config_data['loss_plot'] = os.path.join('./Tests', test_directory, config_data['loss_plot'][2:])
+            config_data['loss_plot'] = os.path.join('./', test_directory, config_data['loss_plot'][2:])
             
             with open(os.path.join(test_directory, 'Config.json'), 'w') as new_config_file:
                 json.dump(config_data, new_config_file, indent=4)
